9690d45654d3f7455e1f347693742fa0c581f4c4
mrylraihan <mr.ylraihan@gmail.com>
2025-04-26 07:49:24 -0700

    Side Effects

---
+++ Section6(Part1SideEffects)/01 Starting Project 5/src/components/Timer.tsx
@@ -0,0 +1,25 @@
+import Container from './UI/Container.tsx';
+import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import { useEffect, useState } from 'react';
+
+export default function Timer({ name, duration }: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+
+  useEffect(()=>{
+    setInterval(()=>{
+      setRemainingTime(prev=>prev-50)
+    },50)
+  },[])
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  
+  return (
+		<Container as='article'>
+			<h2>{name}</h2>
+			<p>
+				<progress max={duration * 1000} value={remainingTime} />
+			</p>
+			<p>{formattedRemainingTime}</p>
+		</Container>
+	)
+}
