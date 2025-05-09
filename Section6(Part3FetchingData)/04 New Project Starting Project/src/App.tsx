9c25fdf6eb57bf89a5fe887876b9f8ecedf7263f
mrylraihan <mr.ylraihan@gmail.com>
2025-05-09 12:14:22 -0700

    Playing with fetching data in useEffect

--- Section6(Part3FetchingData)/04 New Project Starting Project/src/App.tsx
+++ Section6(Part3FetchingData)/04 New Project Starting Project/src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
 import { get } from './util/http'
-import BlogPosts,{BlogPost} from './components/BlogPosts'
+import BlogPosts, { BlogPost } from './components/BlogPosts'
 import fetchingImg from './assets/data-fetching.png'
+import ErrorMessage from './components/ErrorMessage';
 type RawBlogData = {
 	userId: number
 	id: number
@@ -14,7 +15,9 @@ function App() {
 	const [fetchedPost, setFetchedPost] = useState<BlogPost[]>([])
 	useEffect(() => {
 		const fetchPosts = async () => {
-			const res = await get('https://jsonplaceholder.typicode.com/posts') as RawBlogData[]
+			const res = (await get(
+				'https://jsonplaceholdertypicode.com/posts'
+			)) as RawBlogData[]
 			const convertedData: BlogPost[] = res.map((data) => {
 				return {
 					id: data.id,
@@ -22,17 +25,22 @@ function App() {
 					text: data.body,
 				}
 			})
-      setFetchedPost(convertedData)
-
+			setFetchedPost(convertedData)
 		}
 		fetchPosts()
 	}, [])
+  let content :ReactNode;
+  if (fetchedPost.length>0) {
+		content = <BlogPosts posts={fetchedPost} />
+	}else{
+    content = <ErrorMessage text={'Sorry didnt get any data!'}/>
+  }
 	console.log(fetchedPost)
 	return (
 		<main>
 			{/* <h1>Data Fetching!</h1> */}
 			<img src={fetchingImg} alt='image of cat' />
-			<BlogPosts posts={fetchedPost} />
+			{content}
 		</main>
 	)
 }
