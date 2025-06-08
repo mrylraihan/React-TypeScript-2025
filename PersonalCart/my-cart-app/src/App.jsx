a78bdb5bacfd5094362a64b3903e084c0af15e7b
mrylraihan <mr.ylraihan@gmail.com>
2025-06-08 08:10:55 -0700

    Updating Cart Practice

--- PersonalCart/my-cart-app/src/App.jsx
+++ PersonalCart/my-cart-app/src/App.jsx
@@ -15,16 +15,24 @@ function App() {
   console.log("cart",cart)
   console.log("products", products)
   const addToCart = (id, name, price)=>{
-    const isFound = cart.findIndex(item=>item.id == id)
-   console.log("isFound", isFound)
-   setCart(prev=>{
-    const isFound = prev.find(ele=>ele.id==id)
+    // const isFound = cart.findIndex(item=>item.id == id)
+    //  setCart(prev=>{
+      const isFound = cart.find(ele=>ele.id==id)
+      console.log("isFound", isFound)
     if(!isFound){
-      return [...prev, {id, name, price, quantity:1}]
+      setCart(prev=>{return [...prev, {id, name, price, quantity:1}]})
     }else{
-      return prev.map(ele=>ele.id!==id?ele:{...ele, quantity:ele.quantity+1})
+      setCart(prev=>{return prev.map(ele=>ele.id!==id?ele:{...ele, quantity:ele.quantity+1})})
     }
-   })
+  //  })
+  //  setCart(prev=>{
+  //   const isFound = prev.find(ele=>ele.id==id)
+  //   if(!isFound){
+  //     return [...prev, {id, name, price, quantity:1}]
+  //   }else{
+  //     return prev.map(ele=>ele.id!==id?ele:{...ele, quantity:ele.quantity+1})
+  //   }
+  //  })
   }
   const removeFromCart = (id) => {
 		const isFound = cart.find((item) => item.id == id)
